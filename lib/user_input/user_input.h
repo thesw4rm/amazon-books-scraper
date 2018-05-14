19bc12bda9e54c2e89380a68e9cee1924a154c63
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-13 21:21:22 -0400

    Created method declaration for getUserInput() in user_input header

---
+++ lib/user_input/user_input.h
@@ -0,0 +1,17 @@
+/**
+ * Header file to list functions that mess with user input
+ */
+
+#ifndef WEBSCRAPER_MAIN_INPUTS_H
+#define WEBSCRAPER_MAIN_INPUTS_H
+
+/**
+         * Gets user input for URL to scrape.
+         * TODO: Validate input URL
+         *
+         * @return char*
+         * Returns input URL to scrape
+         */
+char *getScrapeLink();
+
+#endif //WEBSCRAPER_MAIN_INPUTS_H
