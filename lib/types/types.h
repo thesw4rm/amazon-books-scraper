83052614e4b43d204911e1afcd78c8dd3f45ef13
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-13 21:20:58 -0400

    Created custom bool type using ints to represent true and false in types header

---
+++ lib/types/types.h
@@ -0,0 +1,12 @@
+//
+// Created by ytpillai on 5/13/18.
+//
+
+#ifndef WEBSCRAPER_TYPES_H
+#define WEBSCRAPER_TYPES_H
+
+typedef int bool;
+#define true 1
+#define false 0
+
+#endif //WEBSCRAPER_TYPES_H
