9c6d7e872843bdae45c5104bdb19bb6a8586684b
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-14 23:35:29 -0400

    Implemented methods and struct in user_input
    Implemented the extractRequestDataFromURL(), changed up the header file a bit to have correct type declarations and function declarations.

--- lib/user_input/user_input.c
+++ lib/user_input/user_input.c
@@ -1,9 +1,75 @@
+#include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
 #include "user_input.h"
 
- /**
+/**
+* TODO: Implement
+* @return request inputted URL
+*/
+char *getScrapeLink() {
+    return "https://www.amazon.com/_/dp/1449355730?tag=oreilly20-20";
+}
+
+REQUEST_HEADER_INFO* extractRequestDataFromURL(char* URL) {
+    printf("Extracting request header data...\n\n");
+    char *host;
+    char *path;
+    bool ssl = false;
+    if (!verifyURL(URL)) {
+        fprintf(stderr, "Error, URL is invalid");
+        exit(-1);
+    }
+
+    //Check for HTTPS or HTTP
+    char requestType[6];
+    strncpy(requestType, URL, 5);
+    requestType[5] = '\0'; //Set null terminating character
+    if (strcmp(requestType, "https") == 0)
+        ssl = true;
+
+
+
+    //Extract host.
+    char *hostStart = strstr(URL, "://") + 3;
+    size_t hostLength = strcspn(hostStart, "/");
+    host = malloc(sizeof(char) * hostLength);
+    strncpy(host, hostStart, hostLength);
+
+    //Extract path, use localPath as placeholder for mallocing the correct size
+    char *localPath = hostStart + hostLength;
+    path = malloc(sizeof(char) * strlen(localPath));
+    strncpy(path, localPath, sizeof(char) * strlen(localPath));
+
+
+
+
+
+
+
+    printf("\tExtracted host: Host = %s", host);
+    printf("\tExtracted path: Path = %s", path);
+    if (ssl)
+        printf("\tExtracted ssl: ssl = true\n");
+    else
+        printf("\tExtracted ssl: ssl = false\n");
+    REQUEST_HEADER_INFO *requestInfo = (REQUEST_HEADER_INFO *) malloc(sizeof(struct requestHeaderInfo));
+    requestInfo->host = host;
+    requestInfo->path = path;
+    requestInfo->ssl = malloc(sizeof(bool));
+    *(requestInfo->ssl) = ssl;
+    return requestInfo;
+}
+
+void freeRequestHeaderInfo(REQUEST_HEADER_INFO* req){
+    free(req->path);
+    free(req->ssl);
+    free(req->host);
+    free(req);
+}
+/**
  * TODO: Implement
- * @return user inputted URL
  */
-char* getScrapeLink(){
-    return "";
+bool verifyURL(char *URL) {
+    return true;
 }
--- lib/user_input/user_input.h
+++ lib/user_input/user_input.h
@@ -5,6 +5,19 @@
 #ifndef WEBSCRAPER_MAIN_INPUTS_H
 #define WEBSCRAPER_MAIN_INPUTS_H
 
+#include "../types/types.h"
+
+/**
+ * @param host- Request host
+ * @param path- Request path
+ * @param ssl- whether to use plain http or negotiate an SSL cert
+ */
+typedef struct requestHeaderInfo {
+    char *host;
+    char *path;
+    bool *ssl;
+} REQUEST_HEADER_INFO;
+
 /**
          * Gets user input for URL to scrape.
          * TODO: Validate input URL
@@ -13,5 +26,25 @@
          * Returns input URL to scrape
          */
 char* getScrapeLink();
+
+/**
+ *
+ * Frees a request header info struct
+ */
+void freeRequestHeaderInfo(REQUEST_HEADER_INFO* req);
+/**
+ * Extracts info of request header from URL
+ * @param URL- input the user's URL
+ * @return struct requestHeaderInfo after extracting relevant information
+ */
+REQUEST_HEADER_INFO* extractRequestDataFromURL(char* URL);
+
+
+/**
+ *
+ * @param URL- user input url
+ * @return verification boolean
+ */
+bool verifyURL(char* URL);
 
 #endif //WEBSCRAPER_MAIN_INPUTS_H
