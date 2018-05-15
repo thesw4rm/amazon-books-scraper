04388436957fc5ae12911afd90c7fa4eca1d4e17
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-15 16:03:24 -0400

    Created functions for noSSL and SSL requests,  resolve domain to ip
    Created noSSL and SSL functions to receive relevant requests and resolve domains to IPs. SSL function is still not complete. getHTMLBody() calls them based on if SSL pointer in struct is true.

--- lib/http/http.c
+++ lib/http/http.c
@@ -7,34 +7,205 @@
 #include <stdlib.h>
 #include "http.h"
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-char *craftRequestHeader(const char *URL, REQUEST_HEADER_INFO *request_header_info) {
-    char *requestHeader = malloc(
-            sizeof(char) * REQUEST_HEADER_MAX_LENGTH); //Create empty request header to fill with sprintf
+char *craftRequestHeader(REQUEST_HEADER_INFO *request_header_info) {
+    char *requestHeader = calloc(REQUEST_HEADER_MAX_LENGTH,
+                                 sizeof(char)); //Create empty request header to fill with snprintf
 
-    snprintf(requestHeader, REQUEST_HEADER_MAX_LENGTH, "GET %s HTTP/1.1 Host: %s", request_header_info->path,
+
+    snprintf(requestHeader, REQUEST_HEADER_MAX_LENGTH, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n", request_header_info->path,
              request_header_info->host);
-    printf("Request header is %s", requestHeader);
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Request header is %s" ANSI_COLOR_RESET, requestHeader);
     return requestHeader;
 }
 
-/**
- * TODO: Implement function
- */
-char *getHTMLBody(const char *URL) {
-    int sockAddr = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); //Only supporting IPV4 right now
+char *noSSLRequest(REQUEST_HEADER_INFO *request_header_info);
 
-    if (sockAddr == -1) {
+int domainToIP(char *domainName, char *ip_string);
+char *SSLRequest(REQUEST_HEADER_INFO *request_header_info);
+
+
+char *getHTMLBody(char *URL) {
+    REQUEST_HEADER_INFO *request_header_info = extractRequestDataFromURL(getScrapeLink());
+    if (*(request_header_info->ssl))
+        return SSLRequest(request_header_info);
+    else
+        return noSSLRequest(request_header_info);
+}
+
+char *SSLRequest(REQUEST_HEADER_INFO *request_header_info) {
+    struct sockaddr_in serverAddress;
+    char *requestHeader;
+    unsigned short serverPort;
+    char serverIP[13];
+    domainToIP(request_header_info->host, serverIP);
+    char *response = calloc(0, 0);
+    ssize_t bytesReceived = 0;
+
+
+    int sockFD; //Only supporting IPV4 right now, returns file descriptor for socket
+
+    if ((sockFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < -1) {
+        freeRequestHeaderInfo(request_header_info);
         fprintf(stderr, "Error, could not open socket in http.c getHTMLBody(). Reason for error %s", strerror(errno));
         exit(-1);
     }
-    printf(ANSI_COLOR_GREEN "Reserved socket space file descriptor %d" ANSI_COLOR_RESET, sockAddr);
+    printf(ANSI_COLOR_GREEN "LOG: Socket file descriptor is %d" ANSI_COLOR_RESET, sockFD);
+    serverPort = 80;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(80);
+    inet_aton(serverIP, &serverAddress.sin_addr);
 
-
-    if (close(sockAddr) == -1) {
-        fprintf(stderr, "Error, could not close socket in http.c getHTMLBody(). Reason for error %s", strerror(errno));
+    if (connect(sockFD, (const struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not connect socket in http.c getHTMLBody(). Reason for error %s",
+                strerror(errno));
         exit(-1);
     }
 
-    return "";
+    printf(ANSI_COLOR_GREEN "\nLOG: Connected socket at descriptor %d to IP %s and port %d" ANSI_COLOR_RESET, sockFD,
+           serverIP, serverPort);
+
+    requestHeader = craftRequestHeader(request_header_info);
+
+    if (send(sockFD, requestHeader, strlen(requestHeader), 0) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not send request. Reason for error %s",
+                strerror(errno));
+        exit(-1);
+    }
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Sent HTTP request from socket at descriptor %d to IP %s and port %d." ANSI_COLOR_RESET,
+           sockFD,
+           serverIP, serverPort);
+
+
+    free(requestHeader);
+    printf(ANSI_COLOR_GREEN "\nLOG: Starting receive operation" ANSI_COLOR_RESET);
+    ssize_t bytesReceivedPrevious = -1;
+    char buffer[RESPONSE_BUFFER_SIZE];
+    while (bytesReceived < (RESPONSE_MAX_LEN * sizeof(char)) && bytesReceived > bytesReceivedPrevious) {
+        bytesReceivedPrevious = bytesReceived;
+        bytesReceived = recv(sockFD, buffer, RESPONSE_BUFFER_SIZE, 0);
+        response = realloc(response, sizeof(response) + RESPONSE_BUFFER_SIZE);
+        strcat(response, buffer); //Append to the end, safe because recv takes care of limiting buffer size
+    }
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Received HTTP response from socket at descriptor %d to IP %s and port %d." ANSI_COLOR_RESET,
+           sockFD,
+           serverIP, serverPort);
+    if (close(sockFD) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not close socket in http.c getHTMLBody(). Reason for error %s", strerror(errno));
+        exit(-1);
+    }
+    printf(ANSI_COLOR_GREEN "\nLOG: Closed socket at descriptor %d" ANSI_COLOR_RESET, sockFD);
+    freeRequestHeaderInfo(request_header_info);
+
+    return response;
 }
+
+/**
+ * TODO: Need to fix
+ */
+char *noSSLRequest(REQUEST_HEADER_INFO *request_header_info) {
+    struct sockaddr_in serverAddress;
+    char *requestHeader;
+    unsigned short serverPort;
+    char serverIP[13];
+    domainToIP(request_header_info->host, serverIP);
+    char *response = calloc(0, 0);
+    ssize_t bytesReceived = 0;
+
+
+    int sockFD; //Only supporting IPV4 right now, returns file descriptor for socket
+
+    if ((sockFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < -1) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not open socket in http.c getHTMLBody(). Reason for error %s", strerror(errno));
+        exit(-1);
+    }
+    printf(ANSI_COLOR_GREEN "LOG: Socket file descriptor is %d" ANSI_COLOR_RESET, sockFD);
+    serverPort = 80;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(80);
+    inet_aton(serverIP, &serverAddress.sin_addr);
+
+    if (connect(sockFD, (const struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not connect socket in http.c getHTMLBody(). Reason for error %s",
+                strerror(errno));
+        exit(-1);
+    }
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Connected socket at descriptor %d to IP %s and port %d" ANSI_COLOR_RESET, sockFD,
+           serverIP, serverPort);
+
+    requestHeader = craftRequestHeader(request_header_info);
+
+    if (send(sockFD, requestHeader, strlen(requestHeader), 0) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not send request. Reason for error %s",
+                strerror(errno));
+        exit(-1);
+    }
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Sent HTTP request from socket at descriptor %d to IP %s and port %d." ANSI_COLOR_RESET,
+           sockFD,
+           serverIP, serverPort);
+
+
+    free(requestHeader);
+    printf(ANSI_COLOR_GREEN "\nLOG: Starting receive operation" ANSI_COLOR_RESET);
+    ssize_t bytesReceivedPrevious = -1;
+    char buffer[RESPONSE_BUFFER_SIZE];
+    while (bytesReceived < (RESPONSE_MAX_LEN * sizeof(char)) && bytesReceived > bytesReceivedPrevious) {
+        bytesReceivedPrevious = bytesReceived;
+        bytesReceived = recv(sockFD, buffer, RESPONSE_BUFFER_SIZE, 0);
+        response = realloc(response, sizeof(response) + RESPONSE_BUFFER_SIZE);
+        strcat(response, buffer); //Append to the end, safe because recv takes care of limiting buffer size
+    }
+
+    printf(ANSI_COLOR_GREEN "\nLOG: Received HTTP response from socket at descriptor %d to IP %s and port %d.\n" ANSI_COLOR_RESET,
+           sockFD,
+           serverIP, serverPort);
+    if (close(sockFD) < 0) {
+        freeRequestHeaderInfo(request_header_info);
+        fprintf(stderr, "Error, could not close socket in http.c getHTMLBody(). Reason for error %s", strerror(errno));
+        exit(-1);
+    }
+    printf(ANSI_COLOR_GREEN "\nLOG: Closed socket at descriptor %d" ANSI_COLOR_RESET, sockFD);
+    freeRequestHeaderInfo(request_header_info);
+
+    return response;
+}
+
+int domainToIP(char *domainName, char *ip_string) {
+    struct hostent *he;
+    struct in_addr **addrlist;
+    if ((he = gethostbyname(domainName)) == NULL) {
+        // get the host info
+        herror("gethostbyname");
+        return 1;
+    }
+
+    addrlist = (struct in_addr **) he->h_addr_list;
+    int i;
+    for (i = 0; addrlist[i] != NULL; i++) {
+        //Return the first one;
+        strcpy(ip_string, inet_ntoa(*addrlist[i]));
+        return 0;
+    }
+
+    return 1;
+
+}
+
+
+
+
--- lib/http/http.h
+++ lib/http/http.h
@@ -8,8 +8,9 @@
 
 #include "../user_input/user_input.h"
 
-#define REQUEST_HEADER_MAX_LENGTH 3000
-
+#define REQUEST_HEADER_MAX_LENGTH 30000
+#define RESPONSE_BUFFER_SIZE 50000
+#define RESPONSE_MAX_LEN 2000000
 
 
 /**
@@ -18,7 +19,7 @@
  * @param path- path after hostname
  * @return char*- formed HTTP request header (GET request). Assume $path as path and $host as host, 'GET $path HTTP/1.1 Host: $host' Headers are appended in format 'Header:Value'
  */
-char* craftRequestHeader(const char *URL, REQUEST_HEADER_INFO* request_header_info);
+char *craftRequestHeader(REQUEST_HEADER_INFO *request_header_info);
 
 
 /**
@@ -27,7 +28,7 @@ char* craftRequestHeader(const char *URL, REQUEST_HEADER_INFO* request_header_in
  * @param bool- whether ssl should be used
  * @return HTML body of response from HTTP/HTTPS request to URL
  */
-char* getHTMLBody(const char *URL);
+char *getHTMLBody(char *URL);
 
 
 #endif //WEBSCRAPER_HTTP_H
