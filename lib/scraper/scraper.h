28d8e81c378b5c47d55a9325cc134147d9ba0c63
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-23 17:44:16 -0400

    Created initial header for scraper.

--- lib/scraper/scraper.h
+++ lib/scraper/scraper.h
@@ -5,4 +5,20 @@
 #ifndef WEBSCRAPER_SCRAPER_H
 #define WEBSCRAPER_SCRAPER_H
 
+typedef struct Element {
+    char *tagName;
+    char *tagContent;
+    int tagLevel;
+    char *tagAttr[];
+} Element;
+
+extern Element *extractedElements[];
+
+void parseHTML(char *html);
+static void addElement(Element *element);
+void searchElement(char *tagName, char *tagAttr[]);
+void freeAll();
+
+
+
 #endif //WEBSCRAPER_SCRAPER_H
