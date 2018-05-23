655f0c5ef42aaa911551335962ea4bd6e4da3b9c
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-23 00:29:33 -0400

    Added ANSI_COLOR_RED for error logging

--- lib/types/types.h
+++ lib/types/types.h
@@ -4,8 +4,9 @@
 
 #ifndef WEBSCRAPER_TYPES_H
 #define WEBSCRAPER_TYPES_H
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+#define ANSI_COLOR_RED      "\x1b[31m"
 
 typedef int bool;
 #define true 1
