0f1ac922c79c4c7a944428f58d63b34487af7513
Yadunandan Pillai <ytpillai@gmail.com>
2018-05-23 00:31:06 -0400

    Fgets to pause execution and code to show pid
    Pause execution using fgets to get strace attached before actually running the program. also, of course, need to print out the PID of the program.

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <memory.h>
 #include <malloc.h>
+#include <zconf.h>
 #include "lib/http/http.h"
 #include "main.h"
 
 int main() {
     printf("Hi\n\n");
+    printf("%ld", (long)getpid());
+    char ha[11];
+    fgets(ha, 10, stdin);
 
     printf(getHTMLBody(getScrapeLink()));
     printf("\n\n\n");
